6afbe2f8c26a3482e2658a0b0a2d14206c20a7d8
Daniel Salgado Rojo <daniel.salgado.rojo@gmail.com>
2017-12-07 17:06:21 +0100

    Add function headers to utils.h. It wot works, compiles,etc

--- queens/lib/utils.h
+++ queens/lib/utils.h
@@ -1,3 +1,15 @@
+// Function headers
+float random_number(float max);
+void shuffle(unsigned int * rows, int n_queens);
+int sum_down(int n);
+unsigned int absolute(int value);
+int factorial(int m);
+void swap(int * vector, int i, int j);
+void permute(int ** permutations, int * vector, int start, int end, int *counter);
+void print_summary(int n_gen, float mean, float st_deviation, int best_score);
+bool print_results(unsigned int scorer);
+
+//Function bodies
 float random_number(float max)
 {
     /*
